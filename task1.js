5ef088c9283abdf8ba85c54ac7f7215e2bbbd740
faco400 <vd.souza1999@gmail.com>
2024-04-23 18:51:56 -0300

    Implement first task and add README

---
+++ task1.js
@@ -0,0 +1,39 @@
+const person = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 30,
+  email: "john.doe@example.com"
+}
+
+Object.keys(person).forEach( prop => {
+  Object.defineProperty(person, prop , {
+    value: person[prop],
+    writable: false,
+  });
+});
+
+person.updateInfo = function(newInfo) {
+  try{
+    Object.keys(newInfo).forEach(key => {
+      if(this.hasOwnProperty(key)) { //if prop exists
+        if(Object.getOwnPropertyDescriptor(this, key).writable === false){ //if is read-only
+          throw new Error(`Cannot update. ${key} is non writable property`);
+        } else { //otherwise add new info
+          this[key] = newInfo[key];
+        }
+      }
+    })
+
+    // Creating address prop
+    Object.defineProperty(person, 'address', {value: {}, enumerable: false, configurable: false});
+
+  }catch(err) {
+    console.log(err);
+  }
+  
+}
+
+// console.log(person)
+// console.log(Object.getOwnPropertyNames(person));
+// person.updateInfo({firstName:'Mac'});
+// console.log(Object.getOwnPropertyDescriptors(person));
