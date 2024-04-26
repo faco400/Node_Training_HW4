92ee4e1da2803a438e5e855bf8ca3d902540aea3
faco400 <vd.souza1999@gmail.com>
2024-04-26 11:33:16 -0300

    implemented transfer method on bank account

--- task3.js
+++ task3.js
@@ -1,25 +1,71 @@
 const bankAccount = {
   _balance: 1000,
+
+  get formattedBalance() {return `$${this._balance}`},
+
+  set balance(newBalance) {
+    if (typeof(newBalance) === 'number' && newBalance >= 0) {
+        this._balance = newBalance;
+    } else {
+        console.log("Invalid balance value.");
+    }
+  }
 }
 
-//getter formattedBalance
-Object.defineProperty(bankAccount, 'formattedBalance', {
-  get: function() {return `$${this._balance}`}
-});
+const targetBankAccount = {
+  _balance: 1000,
 
-//setter
-Object.defineProperty(bankAccount, 'balance', {
-  set: function(newBalance) {
-      if (typeof(newBalance) === 'number' && newBalance >= 0) {
-          this._balance = newBalance;
-      } else {
-          console.log("Invalid balance value.");
-      }
+  get formattedBalance () {return `$${this._balance}`},
+
+  set balance(newBalance) {
+    if (typeof(newBalance) === 'number' && newBalance >= 0) {
+        this._balance = newBalance;
+    } else {
+        console.log("Invalid balance value.");
+    }
   }
-});
+}
+
+// defining setter and getter after object declaration
+// //getter formattedBalance
+// Object.defineProperty(bankAccount, 'formattedBalance', {
+//   get: function() {return `$${this._balance}`}
+// });
+
+// //setter
+// Object.defineProperty(bankAccount, 'balance', {
+//   set: function(newBalance) {
+//       if (typeof(newBalance) === 'number' && newBalance >= 0) {
+//           this._balance = newBalance;
+//       } else {
+//           console.log("Invalid balance value.");
+//       }
+//   }
+// });
+
+bankAccount.transfer = function (currAcount, targetAccount, amount){
+  try {
+    if(currAcount._balance < amount) {
+      throw new Error ('Account 1 has unsuficient balance for transfer');
+    }else {
+      currAcount.balance = currAcount._balance - amount;
+      targetAccount.balance = targetAccount._balance + amount;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 // console.log(Object.getOwnPropertyDescriptors(bankAccount));
 
-// console.log(bankAccount.formattedBalance);
-// bankAccount.balance = 300;
-// console.log(bankAccount.formattedBalance);
+// console.log(bankAccount);
+// console.log(targetBankAccount);
+
+console.log(bankAccount.formattedBalance);
+console.log(targetBankAccount.formattedBalance);
+bankAccount.transfer(bankAccount, targetBankAccount, 400);
+console.log(bankAccount.formattedBalance);
+console.log(targetBankAccount.formattedBalance);
+
+// console.log(bankAccount);
+// console.log(targetBankAccount);
